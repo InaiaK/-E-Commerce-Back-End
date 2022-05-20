e2953d552c53d1d9c1ba5481df0ee62a793fa87f
InaiaKenia <inaiakenia15@gmail.com>
2022-05-19 20:45:12 -0600

    models > Category.js

--- models/Category.js
+++ models/Category.js
@@ -7,15 +7,16 @@ class Category extends Model {}
     // define columns
 Category.init(
   {
-/*
     category_name: {
       // prevents duplicate CATEGORY NAME in DB
       type: DataTypes.STRING,
+      unique: true,
+      notEmpty: true, 
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+
     },
-  },*/
+  },
   {
     sequelize,
     timestamps: false,
